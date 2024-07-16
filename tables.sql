b9a92523fc7622a8a3b9e6cf43eb2bce6066dd3e
priyaa2671 <166091279+priyaa2671@users.noreply.github.com>
2024-07-16 13:26:52 -0500

    Update tables.sql added skill table

--- tables.sql
+++ tables.sql
@@ -7,3 +7,21 @@ CREATE TABLE Education (
     end_date DATE,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+CREATE TABLE Experience (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    company_name VARCHAR(255),
+    role VARCHAR(255),
+    start_date DATE,
+    end_date DATE,
+    email VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+CREATE TABLE Skills (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    email VARCHAR(255),
+    skill_name VARCHAR(255),
+    proficiency_level VARCHAR(50),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
