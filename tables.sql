664a9db4dc28ab83023d3c94944b12c707d8f6a3
priyaa2671 <166091279+priyaa2671@users.noreply.github.com>
2024-07-16 13:28:24 -0500

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -25,3 +25,22 @@ CREATE TABLE Skills (
     proficiency_level VARCHAR(50),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+CREATE TABLE Sessions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    email VARCHAR(255),
+    login_time DATETIME,
+    logout_time DATETIME,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE Certificates (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    email VARCHAR(255),
+    certificate_name VARCHAR(255),
+    issuing_organization VARCHAR(255),
+    issue_date DATE,
+    expiration_date DATE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
